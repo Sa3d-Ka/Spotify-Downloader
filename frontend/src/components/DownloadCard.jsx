fdf1827802325aed715be8d8fb909ebbd286949e
Saad Kanani <saad.kanani.off@gmail.com>
2025-07-20 14:57:36 +0100

    feat: Add DownloadCard component for displaying track download status

---
+++ frontend/src/components/DownloadCard.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const DownloadCard = ({ track }) => {
+  return (
+    <div className="bg-dark py-4 px-4 rounded-md">
+      <div className="flex justify-between items-center mb-4">
+        <div className="flex items-center gap-2.5">
+          <img
+            className="w-14 h-14 rounded"
+            src={track.image || "/fallback.png"}
+            alt="Cover"
+          />
+          <div className="flex flex-col gap-1">
+            <span className="font-bold text-md">{track.title}</span>
+            <span className="text-sm text-gray-400">{track.artist}</span>
+          </div>
+        </div>
+        <p>64%</p>
+      </div>
+      <div className="w-full h-3 rounded-full bg-darkLight ">
+        <div
+          style={{ width: "10%" }}
+          className="h-full bg-primary rounded-full transition-all duration-300"
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default DownloadCard;
--- frontend/src/pages/DownloadPage.jsx
+++ frontend/src/pages/DownloadPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import StepTracker from "../components/StepTracker";
 import { useLocation, useNavigate } from "react-router-dom";
+// import DownloadCard from "../components/DownloadCard";
 
 const DownloadPage = () => {
   const { state } = useLocation();
@@ -22,28 +23,8 @@ const DownloadPage = () => {
       <StepTracker currentStep={3} />
       <div className="flex flex-col gap-4 md:mx-70">
         <h2 className="font-bold text-2xl">Downloading Tracks</h2>
-        <div className="bg-dark py-4 px-4 rounded-md">
-          <div className="flex justify-between items-center mb-4">
-            <div className="flex items-center gap-2.5">
-              <img
-                className="w-14 h-14 rounded"
-                src={track.image || "/fallback.png"}
-                alt="Cover"
-              />
-              <div className="flex flex-col gap-1">
-                <span className="font-bold text-md">{track.title}</span>
-                <span className="text-sm text-gray-400">{track.artist}</span>
-              </div>
-            </div>
-            <p>64%</p>
-          </div>
-          <div className="w-full h-3 rounded-full bg-darkLight ">
-            <div
-              style={{ width: "10%" }}
-              className="h-full bg-primary rounded-full transition-all duration-300"
-            ></div>
-          </div>
-        </div>
+        {/* <DownloadCard track={track} /> */}
+        
       </div>
     </div>
   );
